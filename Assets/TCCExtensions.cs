90018ef2d635bd1fdd7d14bed6d2aba422cf0cc8
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-05-18 02:17:57 +0900

    Add CC0 simple humanoid and TccExtensions

---
+++ Assets/TCCExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Unity.TinyCharacterController.Control;
+
+namespace PlayerControl
+{
+    public static class TCCExtensions
+    {
+        public static bool IsDoubleJump(this JumpControl jumpControl)
+        {
+            return jumpControl.AerialJumpCount >= 1;
+        }
+    }
+}
