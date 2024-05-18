b0e2b278792dc79eca2e7e3033fcfaae5f31d704
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-05-19 02:04:03 +0900

    Player movements are now controlled by scripts

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,102 @@
+using Unity.TinyCharacterController.Check;
+using Unity.TinyCharacterController.Control;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PlayerControl
+{
+    /// <summary>
+    /// Player controller.
+    /// </summary>
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(PlayerInput))]
+    [RequireComponent(typeof(MoveControl))]
+    [RequireComponent(typeof(JumpControl))]
+    [RequireComponent(typeof(GroundCheck))]
+    [RequireComponent(typeof(CursorPositionControl))]
+    public sealed class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private Animator animator;
+
+        [SerializeField]
+        private PlayerInput playerInput;
+
+        [SerializeField]
+        private MoveControl moveControl;
+
+        [SerializeField]
+        private JumpControl jumpControl;
+
+        [SerializeField]
+        private GroundCheck groundCheck;
+
+        [SerializeField]
+        private CursorPositionControl cursorPosControl;
+
+        private readonly int speedAnim = Animator.StringToHash("Speed");
+
+        private readonly int groundAnim = Animator.StringToHash("IsGround");
+
+        private readonly int jumpStartAnim = Animator.StringToHash("JumpStart");
+
+        private readonly int doubleJumpAnim = Animator.StringToHash("DoubleJump");
+
+        private readonly int forwardAnim = Animator.StringToHash("Forward");
+
+        private readonly int sideStepAnim = Animator.StringToHash("SideStep");
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Start()
+        {
+            playerInput.onActionTriggered += context =>
+            {
+                const string moveAction = "Move";
+                const string sprintAction = "Sprint";
+                const string jumpAction = "Jump";
+                const string mousePosAction = "MousePosition";
+
+                const int heightPriority = 10;
+
+                switch (context.action.name)
+                {
+                    case moveAction when context.phase is InputActionPhase.Performed or InputActionPhase.Canceled:
+                        moveControl.Move(context.ReadValue<Vector2>());
+                        break;
+                    case sprintAction when context.phase is InputActionPhase.Performed:
+                        moveControl.TurnPriority = heightPriority;
+                        const float sprintHoldSpeed = 4.0f;
+                        moveControl.MoveSpeed = sprintHoldSpeed;
+                        break;
+                    case sprintAction when context.phase is InputActionPhase.Canceled:
+                        moveControl.TurnPriority = default;
+                        cursorPosControl.TurnPriority = default;
+                        const float sprintReleasedSpeed = 1.2f;
+                        moveControl.MoveSpeed = sprintReleasedSpeed;
+                        break;
+                    case jumpAction when context.phase is InputActionPhase.Started:
+                        jumpControl.Jump();
+                        break;
+                    case mousePosAction when context.phase is InputActionPhase.Performed:
+                        cursorPosControl.LookTargetPoint(context.ReadValue<Vector2>());
+                        cursorPosControl.TurnPriority = heightPriority;
+                        break;
+                }
+            };
+
+            jumpControl.OnJump.AddListener(() => animator.Play(jumpControl.IsDoubleJump() ? doubleJumpAnim : jumpStartAnim));
+        }
+
+        private void Update()
+        {
+            animator.SetFloat(speedAnim, moveControl.CurrentSpeed);
+            animator.SetBool(groundAnim, groundCheck.IsOnGround);
+
+            Vector3 currentDirection = moveControl.LocalDirection;
+            const float dampTime = 0.1f;
+            float deltaTime = Time.deltaTime;
+            animator.SetFloat(forwardAnim, currentDirection.z, dampTime, deltaTime);
+            animator.SetFloat(sideStepAnim, currentDirection.x, dampTime, deltaTime);
+        }
+    }
+}
--- Assets/TCCExtensions.cs
+++ Assets/TCCExtensions.cs
@@ -1,9 +1,11 @@
-﻿using Unity.TinyCharacterController.Control;
+﻿using System.Runtime.CompilerServices;
+using Unity.TinyCharacterController.Control;
 
 namespace PlayerControl
 {
     public static class TCCExtensions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsDoubleJump(this JumpControl jumpControl)
         {
             return jumpControl.AerialJumpCount >= 1;
