14773db19bc86c5a3a05cb9e81a6eeebd9f233de
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-07-01 23:45:13 +0900

    Restrict assignment timing of readonly field variables so that they are always after Awake
    (cherry picked from commit 680e4c9a6bddfeb2222e9465994a7729b2f006ba)

---
+++ Packages/PlayerControl/Runtime/AnimHashConstant.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace PlayerControl
+{
+    /// <summary>
+    /// Animation hash constant.
+    /// </summary>
+    internal sealed class AnimHashConstant
+    {
+        /// <summary>
+        /// "Speed" animation hash.
+        /// </summary>
+        internal readonly int SpeedAnim = Animator.StringToHash("Speed");
+
+        /// <summary>
+        /// "IsGround" animation hash.
+        /// </summary>
+        internal readonly int GroundAnim = Animator.StringToHash("IsGround");
+
+        /// <summary>
+        /// "JumpStart" animation hash.
+        /// </summary>
+        internal readonly int JumpStartAnim = Animator.StringToHash("JumpStart");
+
+        /// <summary>
+        /// "DoubleJump" animation hash.
+        /// </summary>
+        internal readonly int DoubleJumpAnim = Animator.StringToHash("DoubleJump");
+
+        /// <summary>
+        /// "Forward" animation hash.
+        /// </summary>
+        internal readonly int ForwardAnim = Animator.StringToHash("Forward");
+
+        /// <summary>
+        /// "SideStep" animation hash.
+        /// </summary>
+        internal readonly int SideStepAnim = Animator.StringToHash("SideStep");
+
+        // Ensure that this class is not generated before Awake.
+        internal AnimHashConstant(in Object obj)
+        {
+        }
+    }
+}
