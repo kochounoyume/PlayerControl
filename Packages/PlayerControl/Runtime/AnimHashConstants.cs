6f28b6e6815f546d65186d1177319a50d68bcbee
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2024-07-02 13:52:53 +0900

    Update to add ToString in AnimHashConstants.cs
    (cherry picked from commit 0f3594d0c139c1e79967ded6000aae50b8079b51)

--- Packages/PlayerControl/Runtime/AnimHashConstants.cs
+++ Packages/PlayerControl/Runtime/AnimHashConstants.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+using System.Text;
+using UnityEngine;
 
 namespace PlayerControl
 {
@@ -50,6 +51,37 @@ namespace PlayerControl
             DoubleJumpAnim = Animator.StringToHash("DoubleJump");
             ForwardAnim = Animator.StringToHash("Forward");
             SideStepAnim = Animator.StringToHash("SideStep");
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder(nameof(AnimHashConstants));
+            builder.Append(" {");
+            builder.Append(nameof(SpeedAnim));
+            builder.Append(" = ");
+            builder.Append(SpeedAnim);
+            builder.Append(", ");
+            builder.Append(nameof(GroundAnim));
+            builder.Append(" = ");
+            builder.Append(GroundAnim);
+            builder.Append(", ");
+            builder.Append(nameof(JumpStartAnim));
+            builder.Append(" = ");
+            builder.Append(JumpStartAnim);
+            builder.Append(", ");
+            builder.Append(nameof(DoubleJumpAnim));
+            builder.Append(" = ");
+            builder.Append(DoubleJumpAnim);
+            builder.Append(", ");
+            builder.Append(nameof(ForwardAnim));
+            builder.Append(" = ");
+            builder.Append(ForwardAnim);
+            builder.Append(", ");
+            builder.Append(nameof(SideStepAnim));
+            builder.Append(" = ");
+            builder.Append(SideStepAnim);
+            builder.Append(" }");
+            return builder.ToString();
         }
     }
 }
