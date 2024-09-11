7ac2ecb0069fd952b61cb68f7ba2c52b6a16284f
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-09-12 00:36:03 +0900

    Remove StringBuilder and use string.Create in AnimHashConstants.ToString()

--- Packages/PlayerControl/Runtime/AnimHashConstants.cs
+++ Packages/PlayerControl/Runtime/AnimHashConstants.cs
@@ -1,4 +1,5 @@
-using System.Text;
+using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace PlayerControl
@@ -50,33 +51,74 @@ namespace PlayerControl
 
         public override string ToString()
         {
-            StringBuilder builder = new StringBuilder(nameof(AnimHashConstants));
-            builder.Append(" {");
-            builder.Append(nameof(Speed));
-            builder.Append(" = ");
-            builder.Append(Speed);
-            builder.Append(", ");
-            builder.Append(nameof(IsGround));
-            builder.Append(" = ");
-            builder.Append(IsGround);
-            builder.Append(", ");
-            builder.Append(nameof(JumpStart));
-            builder.Append(" = ");
-            builder.Append(JumpStart);
-            builder.Append(", ");
-            builder.Append(nameof(DoubleJump));
-            builder.Append(" = ");
-            builder.Append(DoubleJump);
-            builder.Append(", ");
-            builder.Append(nameof(Forward));
-            builder.Append(" = ");
-            builder.Append(Forward);
-            builder.Append(", ");
-            builder.Append(nameof(SideStep));
-            builder.Append(" = ");
-            builder.Append(SideStep);
-            builder.Append(" }");
-            return builder.ToString();
+            int speed = GetDigits(Speed);
+            int isGround = GetDigits(IsGround);
+            int jumpStart = GetDigits(JumpStart);
+            int doubleJump = GetDigits(DoubleJump);
+            int forward = GetDigits(Forward);
+            int sideStep = GetDigits(SideStep);
+            int totalDigits = speed + isGround + jumpStart + doubleJump + forward + sideStep + 97;
+
+            return string.Create(totalDigits, this, static (span, constants) =>
+            {
+                ReadOnlySpan<char> equals = stackalloc char[] { ' ', '=', ' ' };
+                ReadOnlySpan<char> comma = stackalloc char[] { ',', ' ' };
+
+                nameof(AnimHashConstants).AsSpan().CopyTo(span);
+                int length = nameof(AnimHashConstants).Length;
+                stackalloc char[] { ' ', '{', ' ' }.CopyTo(span.Slice(length));
+                length += 3;
+                nameof(Speed).AsSpan().CopyTo(span.Slice(length));
+                length += nameof(Speed).Length;
+                equals.CopyTo(span.Slice(length));
+                length += equals.Length;
+                constants.Speed.TryFormat(span.Slice(length), out int speedWritten);
+                length += speedWritten;
+                comma.CopyTo(span.Slice(length));
+                length += comma.Length;
+                nameof(IsGround).AsSpan().CopyTo(span.Slice(length));
+                length += nameof(IsGround).Length;
+                equals.CopyTo(span.Slice(length));
+                length += equals.Length;
+                constants.IsGround.TryFormat(span.Slice(length), out int isGroundWritten);
+                length += isGroundWritten;
+                comma.CopyTo(span.Slice(length));
+                length += comma.Length;
+                nameof(JumpStart).AsSpan().CopyTo(span.Slice(length));
+                length += nameof(JumpStart).Length;
+                equals.CopyTo(span.Slice(length));
+                length += equals.Length;
+                constants.JumpStart.TryFormat(span.Slice(length), out int jumpStartWritten);
+                length += jumpStartWritten;
+                comma.CopyTo(span.Slice(length));
+                length += comma.Length;
+                nameof(DoubleJump).AsSpan().CopyTo(span.Slice(length));
+                length += nameof(DoubleJump).Length;
+                equals.CopyTo(span.Slice(length));
+                length += equals.Length;
+                constants.DoubleJump.TryFormat(span.Slice(length), out int doubleJumpWritten);
+                length += doubleJumpWritten;
+                comma.CopyTo(span.Slice(length));
+                length += comma.Length;
+                nameof(Forward).AsSpan().CopyTo(span.Slice(length));
+                length += nameof(Forward).Length;
+                equals.CopyTo(span.Slice(length));
+                length += equals.Length;
+                constants.Forward.TryFormat(span.Slice(length), out int forwardWritten);
+                length += forwardWritten;
+                comma.CopyTo(span.Slice(length));
+                length += comma.Length;
+                nameof(SideStep).AsSpan().CopyTo(span.Slice(length));
+                length += nameof(SideStep).Length;
+                equals.CopyTo(span.Slice(length));
+                length += equals.Length;
+                constants.SideStep.TryFormat(span.Slice(length), out int sideStepWritten);
+                length += sideStepWritten;
+                stackalloc char[] { ' ', '}' }.CopyTo(span.Slice(length));
+            });
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            static int GetDigits(in int value) => (int)MathF.Log10(Math.Abs(value)) + 1 + (value < 0 ? 1 : 0);
         }
     }
 }
